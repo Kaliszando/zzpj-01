8faa4104ac9e1908067594d2353c67ca0771fec8
Kaliszando <kaliszadam99@gmail.com>
2021-03-09 22:55:41 +0100

    lombok dependency and usage

--- src/main/java/com/pl/p/zzpj/App.java
+++ src/main/java/com/pl/p/zzpj/App.java
@@ -1,13 +1,16 @@
 package com.pl.p.zzpj;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello mvn!" );
+        User user = new User();
+        user.setAge(10);
+        user.setName("John");
+
+        System.out.println(user.getAge());
+        System.out.println(user.getName());
     }
 }
---
+++ src/main/java/com/pl/p/zzpj/User.java
@@ -0,0 +1,13 @@
+package com.pl.p.zzpj;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+
+    private String name;
+    private String password;
+    private int age;
+}
